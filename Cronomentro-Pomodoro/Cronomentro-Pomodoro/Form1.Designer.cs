9a13b91759a69f27586d48a6dd90bf906e0044b6
NetaoFrancisco <cirilonetao@gmail.com>
2017-06-12 20:17:33 -0300

    Add files via upload

---
+++ Cronomentro-Pomodoro/Cronomentro-Pomodoro/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Cronomentro_Pomodoro
+{
+    partial class Cronometro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cronometro));
+            this.lblHoras = new System.Windows.Forms.Label();
+            this.lblSegundos = new System.Windows.Forms.Label();
+            this.lblMinutos = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtSegundos = new System.Windows.Forms.TextBox();
+            this.txtMinutos = new System.Windows.Forms.TextBox();
+            this.lblCronomentro = new System.Windows.Forms.Label();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLimparHoras = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblHoras
+            // 
+            this.lblHoras.AutoSize = true;
+            this.lblHoras.Font = new System.Drawing.Font("Lucida Handwriting", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoras.Location = new System.Drawing.Point(40, 60);
+            this.lblHoras.Name = "lblHoras";
+            this.lblHoras.Size = new System.Drawing.Size(75, 21);
+            this.lblHoras.TabIndex = 0;
+            this.lblHoras.Text = "Horas:";
+            this.lblHoras.Click += new System.EventHandler(this.lblHoras_Click);
+            // 
+            // lblSegundos
+            // 
+            this.lblSegundos.AutoSize = true;
+            this.lblSegundos.Font = new System.Drawing.Font("Lucida Handwriting", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSegundos.Location = new System.Drawing.Point(307, 60);
+            this.lblSegundos.Name = "lblSegundos";
+            this.lblSegundos.Size = new System.Drawing.Size(110, 21);
+            this.lblSegundos.TabIndex = 1;
+            this.lblSegundos.Text = "Segundos:";
+            // 
+            // lblMinutos
+            // 
+            this.lblMinutos.AutoSize = true;
+            this.lblMinutos.Font = new System.Drawing.Font("Lucida Handwriting", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinutos.Location = new System.Drawing.Point(161, 60);
+            this.lblMinutos.Name = "lblMinutos";
+            this.lblMinutos.Size = new System.Drawing.Size(96, 21);
+            this.lblMinutos.TabIndex = 2;
+            this.lblMinutos.Text = "Minutos:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(56, 84);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(33, 30);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // txtSegundos
+            // 
+            this.txtSegundos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSegundos.Location = new System.Drawing.Point(343, 84);
+            this.txtSegundos.Multiline = true;
+            this.txtSegundos.Name = "txtSegundos";
+            this.txtSegundos.Size = new System.Drawing.Size(34, 30);
+            this.txtSegundos.TabIndex = 4;
+            this.txtSegundos.TextChanged += new System.EventHandler(this.txtSegundos_TextChanged);
+            this.txtSegundos.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSegundos_KeyPress);
+            // 
+            // txtMinutos
+            // 
+            this.txtMinutos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMinutos.Location = new System.Drawing.Point(191, 84);
+            this.txtMinutos.Multiline = true;
+            this.txtMinutos.Name = "txtMinutos";
+            this.txtMinutos.Size = new System.Drawing.Size(34, 30);
+            this.txtMinutos.TabIndex = 5;
+            this.txtMinutos.TextChanged += new System.EventHandler(this.txtMinutos_TextChanged);
+            this.txtMinutos.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMinutos_KeyPress);
+            // 
+            // lblCronomentro
+            // 
+            this.lblCronomentro.AutoSize = true;
+            this.lblCronomentro.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCronomentro.Location = new System.Drawing.Point(170, 182);
+            this.lblCronomentro.Name = "lblCronomentro";
+            this.lblCronomentro.Size = new System.Drawing.Size(78, 31);
+            this.lblCronomentro.TabIndex = 6;
+            this.lblCronomentro.Text = "0000";
+            this.lblCronomentro.Click += new System.EventHandler(this.lblCronomentro_Click);
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicio.ForeColor = System.Drawing.Color.Black;
+            this.btnInicio.Location = new System.Drawing.Point(115, 328);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(88, 46);
+            this.btnInicio.TabIndex = 7;
+            this.btnInicio.Text = "Inicio";
+            this.btnInicio.UseVisualStyleBackColor = false;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.Color.Black;
+            this.btnStop.Location = new System.Drawing.Point(253, 328);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(83, 46);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Parar";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Handwriting", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 27);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Cronomentro Pomodoro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(111, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 21);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Contador em segundos:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.BackgroundImage = global::Cronomentro_Pomodoro.Properties.Resources.pomodoro1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(148, 216);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 106);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnLimparHoras
+            // 
+            this.btnLimparHoras.Location = new System.Drawing.Point(56, 131);
+            this.btnLimparHoras.Name = "btnLimparHoras";
+            this.btnLimparHoras.Size = new System.Drawing.Size(45, 29);
+            this.btnLimparHoras.TabIndex = 12;
+            this.btnLimparHoras.Text = "Limpar";
+            this.btnLimparHoras.UseVisualStyleBackColor = true;
+            this.btnLimparHoras.Click += new System.EventHandler(this.btnLimparHoras_Click);
+            // 
+            // Cronometro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 405);
+            this.Controls.Add(this.btnLimparHoras);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.lblCronomentro);
+            this.Controls.Add(this.txtMinutos);
+            this.Controls.Add(this.txtSegundos);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblMinutos);
+            this.Controls.Add(this.lblSegundos);
+            this.Controls.Add(this.lblHoras);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Cronometro";
+            this.Text = "Cronomentro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHoras;
+        private System.Windows.Forms.Label lblSegundos;
+        private System.Windows.Forms.Label lblMinutos;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtSegundos;
+        private System.Windows.Forms.TextBox txtMinutos;
+        private System.Windows.Forms.Label lblCronomentro;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnLimparHoras;
+    }
+}
+
---
+++ Cronomentro-Pomodoro/Cronomentro-Pomodoro/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cronomentro_Pomodoro
+{
+    public partial class Cronometro : Form
+    {
+
+        int segundos = 0;
+        int horas = 0;
+        int minutos = 0;
+        int soma;
+
+
+        public Cronometro()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            horas = Convert.ToInt32(textBox1.Text);
+            soma = soma + (horas * 3600);
+
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            timer1.Start();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+        }
+
+        private void txtMinutos_TextChanged(object sender, EventArgs e)
+        {
+            minutos = Convert.ToInt32(txtMinutos.Text);
+            soma = soma + (minutos * 60);
+
+
+        }
+
+        private void txtSegundos_TextChanged(object sender, EventArgs e)
+        {
+            segundos = Convert.ToInt32(txtSegundos.Text);
+            soma = soma + segundos;
+
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            soma = soma - 1;
+
+            lblCronomentro.Text = Convert.ToString(soma);
+            if (soma == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Seu tempo acabou,agora faça a pausa de 5 minutos e faça outro objetivo ");
+
+            }
+
+
+        }
+
+        private void lblCronomentro_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            {
+                {
+                    //Se a tecla digitada não for número e nem backspace
+                    if (!char.IsDigit(e.KeyChar))
+                    {
+                        //Atribui True no Handled para cancelar o evento
+                        e.Handled = true;
+                    }
+                }
+            }
+        }
+
+        private void txtMinutos_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            {
+
+                if (!char.IsDigit(e.KeyChar))
+                {
+
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void txtSegundos_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            {
+                //Se a tecla digitada não for número e nem backspace
+                if (!char.IsDigit(e.KeyChar))
+                {
+                    //Atribui True no Handled para cancelar o evento
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void lblHoras_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimparHoras_Click(object sender, EventArgs e)
+        {
+
+
+            
+
+        }
+       
+    }
+ }
